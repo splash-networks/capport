06baed27890b68573cb41d19f9f986475fbe15d9
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2020-01-27 10:49:15 +0500

    Update connecting.php

--- NoDogSplash/connecting.php
+++ NoDogSplash/connecting.php
@@ -11,7 +11,7 @@ $redir = $_SESSION["redir"];
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Free WiFi</title>
+  <title>WiFi Portal</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="stylesheet" href="bulma.min.css" />
   <script defer src="fontawesome-free-5.3.1-web\js\all.js"></script>
