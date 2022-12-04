5c21d0be0ba2e4d9044e5f555a9cd92f10e98bf9
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2022-12-04 11:06:28 +0500

    Update index.php

--- Draytek/index.php
+++ Draytek/index.php
@@ -1,29 +1,21 @@
 <?php
-session_start();
-
-if(isset($_GET['loginurl'])) {
-  $_SESSION['loginurl'] = $_GET['loginurl'];
-}
 
 if (isset($_POST['submit'])) {
-    $loginurl = $_SESSION['loginurl'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-    header("Location: " . $loginurl . "?username=" . $username . "&password=" . $password);
+  header("Location: http://portal.draytek.com:8001/cgi-bin/wifilogin.cgi?username=".$username."&password=".$password);
 }
 ?>
 
 <html>
 <body>
-
-<form name="login" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "POST">
-<label for="username">Username</label>
-<input class="input" type="text" id="username" name="username" />
-<label for="password">Password</label>
-<input class="input" type="text" id="password" name="password" />
-<input type="submit" name="submit" value="LOGIN" />
-</form> 
-
+    <form name="login" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "POST">
+        <label for="username">Username</label>
+        <input class="input" type="text" id="username" name="username" />
+        <label for="password">Password</label>
+        <input class="input" type="text" id="password" name="password" />
+        <input type="submit" name="submit" value="LOGIN" />
+    </form>
 </body>
 </html>
