f8cc84edc385d4eda1a58a186825bcb7ad9695ad
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2023-01-19 06:09:02 +0500

    Update index.php

--- Ruckus/index.php
+++ Ruckus/index.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+
+$submitUrl = "http://" . $_GET['sip'] . ":9997/login";
+
 ?>
 
 <!doctype html>
@@ -10,48 +12,13 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 <body>
-
-<?php
-	$res = $_GET["res"];
-	if ($res === "notyet")
-	{
-?>
 	<p>Welcome!<br>
 	Please login to our Wifi service</p>
 
-	<form method="POST" action="connect.php">
-  	<label>User Name: <input type="text" name="username"></label><br/>
-  	<label>Password: <input type="text" name="password"></label><br/>
-  	<input type="hidden" name="challenge" value="<?php echo $_GET["challenge"] ?>">
-  	<input type="hidden" name="uamip" value="<?php echo $_GET["uamip"] ?>">
-  	<input type="hidden" name="uamport" value="<?php echo $_GET["uamport"] ?>">
-  	<input type="hidden" name="userurl" value="<?php echo $_GET["userurl"] ?>">
-  	<input type="submit">
+	<form method="POST" action="<?php echo htmlspecialchars($submitUrl); ?>">
+	<label>User Name: <input type="text" name="username"></label><br/>
+	<label>Password: <input type="text" name="password"></label><br/>
+	<input type="submit">
 	</form>
-
-<?php
-	}
-	else if ($res === "success") {
-		$redir = $_SESSION["userurl"];
-	  	if(isset($redir)) {
-	  		echo "<head>";
-    		echo '<meta http-equiv="refresh" content="3;URL=\'' . $redir . '\'">';
-    		echo "</head>";
-    	}
-    	else {
-    		echo "<h2>Log-in successful!</h2>";
-    	}
-	}
-	else if ($res === "failed") {
-		echo "<h2>Whoops, failed to authenticate</h2>";
-	}
-	else if ($res === "logoff") {
-		echo "<h2>Logging off ...</h2>";
-	}
-	else {
-		echo "<h2>Oops!, bad 'res' parameter</h2>";
-	}
-?>
-
 </body>
 </html>
